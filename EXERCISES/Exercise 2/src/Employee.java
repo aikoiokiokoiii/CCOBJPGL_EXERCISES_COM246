bde36f985fbcabfdc77c3080f4f76edfd0bb001d
Andrew Sañosa <sanosaandrew@gmail.com>
2025-04-03 18:55:38 +0800

    Add files via upload

---
+++ EXERCISES/Exercise 2/src/Employee.java
@@ -0,0 +1,13 @@
+class Employee {
+
+    protected String work;
+    protected int salary;
+    protected String name;
+
+     public  String getWork(){
+             return this.work;
+          }
+     public  int getSalary(){
+             return this.salary;
+          }
+}
